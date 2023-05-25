b4890d0be470442ec4b2eaeff7d041cbaa4bf1aa
eddi <108631435+LowLevelLoser@users.noreply.github.com>
2023-05-24 20:16:44 -0500

    Delete blockade directory

--- blockade/build.sh
+++
@@ -1,1 +0,0 @@
-cc -o blockade main.c rendering.c logic.c minimax.c -s -Wall -lraylib -lm -lpthread -ldl -lrt -lX11
--- blockade/minimax.h
+++
@@ -1,8 +0,0 @@
-#ifndef  MINIMAX_H_
-#define MINIMAX_H_
-
-#include "game.h"
-
-void RunAi(game_t *game, player_t *red, player_t *blue, player_t *gray);
-
-#endif // !DEBUG
