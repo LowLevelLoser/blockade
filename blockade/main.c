4a71e9f370a16f197d933f6ed39ecf6198c80f53
eddi <108631435+LowLevelLoser@users.noreply.github.com>
2023-05-24 20:15:00 -0500

    Add files via upload

---
+++ blockade/main.c
@@ -0,0 +1,53 @@
+#include <raylib.h>
+
+#include "game.h"
+#include "rendering.h"
+#include "logic.h"
+
+int main(){
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Blockade");
+    SetExitKey(0);
+    SetTargetFPS(30);
+
+
+    player_t red_p;
+    red_p.p_counter = 0;
+    red_p.p_moves = 16;
+
+    player_t blue_p;
+    blue_p.p_counter = 0;
+    blue_p.p_moves = 16;
+    blue_p.headX = 0;
+    blue_p.headY = 0;
+
+    player_t gray_p;
+    gray_p.p_counter = 0;
+    gray_p.p_moves = 16;
+
+    game_t game = 
+    {
+        .board = {{EMPTY}},
+        .turn = P_RED,
+        .state = MENU_STATE,
+        .choice = 2,
+        .red_player = &red_p,
+        .blue_player = &blue_p,
+        .gray_player = &gray_p,
+    };
+
+    while(!WindowShouldClose()){
+        RunGame(&game);
+        BeginDrawing();
+            ClearBackground(RAYWHITE);
+            RenderGame(&game);
+        EndDrawing();
+    }
+    
+    //StopMusicStream(game.music);
+
+    //CloseAudioDevice();
+
+    CloseWindow();
+
+    return 0;
+}
